01f621e3edb3812e7ed87b615d0fb56594708d52
timur612 <tz220404@gmail.com>
2022-11-25 12:02:35 +0300

    video controls

--- src/lib-components/vue-webrtc.vue
+++ src/lib-components/vue-webrtc.vue
@@ -4,7 +4,7 @@
              v-bind:video="item"
              v-bind:key="item.id"
              class="video-item">
-            <video controls autoplay playsinline ref="videos" :height="cameraHeight" :muted="item.muted" :id="item.id"></video>
+            <video autoplay playsinline ref="videos" :height="cameraHeight" :muted="item.muted" :id="item.id"></video>
         </div>
     </div>
 </template>
